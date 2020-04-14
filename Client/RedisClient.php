a24ffb4728fbf69434c228b6b2a841ea3ac11dba
Mohamed Bouziane <34442510+medbouziane@users.noreply.github.com>
2020-04-14 15:36:59 +0100

    Update RedisClient.php

--- Client/RedisClient.php
+++ Client/RedisClient.php
@@ -84,7 +84,7 @@ class RedisClient
     public function set($key, $value)
     {
         //return $this->connection->set($key, $value);
-        return $this->connection->setex($key, 43200, $value);
+        return $this->connection->setex($key, "43200", $value);
     }
 
     /**
